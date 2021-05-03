66b1d493983ef62589041ebcf65b9c7d516be090
imad <imad.elfetouh@hotmail.com>
2021-05-03 15:56:24 +0200

    cookie

--- profileservice/src/main/java/com/imadelfetouh/profileservice/security/CookieFilter.java
+++ profileservice/src/main/java/com/imadelfetouh/profileservice/security/CookieFilter.java
@@ -9,9 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.logging.Logger;
 
 @Component
 public class CookieFilter implements Filter {
+
+    private static final Logger logger = Logger.getLogger(CookieFilter.class.getName());
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
@@ -23,6 +26,7 @@ public class CookieFilter implements Filter {
         if(cookies != null) {
             Cookie cookie = Arrays.stream(cookies).filter(c -> c.getName().equals("jwt-token")).findFirst().orElse(null);
             if(cookie == null) {
+                logger.info("Cookie jwt-token not found");
                 httpServletResponse.setStatus(401);
                 return;
             }
@@ -33,6 +37,9 @@ public class CookieFilter implements Filter {
                 filterChain.doFilter(httpServletRequest, httpServletResponse);
                 return;
             }
+        }
+        else{
+            logger.info("No cookies found");
         }
 
         httpServletResponse.setStatus(401);

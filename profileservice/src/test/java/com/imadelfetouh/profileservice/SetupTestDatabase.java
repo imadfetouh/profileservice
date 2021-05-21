d11984e6f35018a5e876e54d7e2016c7f6319aa9
imad <imad.elfetouh@hotmail.com>
2021-05-21 13:35:02 +0200

    bigs

--- profileservice/src/test/java/com/imadelfetouh/profileservice/SetupTestDatabase.java
+++ profileservice/src/test/java/com/imadelfetouh/profileservice/SetupTestDatabase.java
@@ -2,7 +2,6 @@ package com.imadelfetouh.profileservice;
 
 import com.imadelfetouh.profileservice.dal.configuration.QueryExecuter;
 import com.imadelfetouh.profileservice.dal.ormmodel.Profile;
-import com.imadelfetouh.profileservice.dal.ormmodel.Tweet;
 import com.imadelfetouh.profileservice.dal.ormmodel.User;
 import com.imadelfetouh.profileservice.model.response.ResponseModel;
 import org.hibernate.Session;
